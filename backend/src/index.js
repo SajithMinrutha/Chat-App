d0f1220380b25594e7929405df99eecd3084ef4d
Sajith Minrutha <minruthasajith@gmail.com>
2025-05-05 21:41:50 -0600

    basic backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+
+const APP = express();
+const PORT = process.env.PORT || 5002;
+
+APP.use("/api/auth", authRoutes);
+
+APP.listen(PORT, () => {
+  console.log(`Server Is Running On Port ${PORT}`);
+});
